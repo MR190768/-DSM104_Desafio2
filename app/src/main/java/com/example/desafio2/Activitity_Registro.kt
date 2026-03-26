9af06b174762ef80d94291d38744e5b2c57ba261
MR190768 <mr190768@alumno.udb.edu.sv>
2026-03-25 18:47:46 -0600

    ADD-conexion con firebase auth login y registro

--- app/src/main/java/com/example/desafio2/Activitity_Registro.kt
+++ app/src/main/java/com/example/desafio2/Activitity_Registro.kt
@@ -3,13 +3,18 @@ package com.example.desafio2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class Activitity_Registro : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,11 +25,37 @@ class Activitity_Registro : AppCompatActivity() {
             insets
         }
 
+        auth= FirebaseAuth.getInstance()
+
+
         findViewById<TextView>(R.id.tv_alreadyAccaount).setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
+        findViewById<Button>(R.id.btn_regiser).setOnClickListener {
+            val email=findViewById<EditText>(R.id.ed_email).text.toString()
+            val password=findViewById<EditText>(R.id.ed_password).text.toString()
+            registerUser(email,password)
+        }
 
+
+    }
+
+    private fun registerUser(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){task ->
+                if (task.isSuccessful){
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                }.addOnFailureListener{e->
+                Toast.makeText(
+                    applicationContext,
+                    e.localizedMessage,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
     }
 }
--- app/src/main/java/com/example/desafio2/MainActivity.kt
+++ app/src/main/java/com/example/desafio2/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.desafio2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,9 +22,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        auth= FirebaseAuth.getInstance()
+
+
         findViewById<Button>(R.id.btn_signIn).setOnClickListener {
             val intent = Intent(this, Activitity_Registro::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun loginUser(email: String, password: String){
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){task ->
+                if (task.isSuccessful){
+                    val intent = Intent(this, Activity_menu::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }.addOnFailureListener{e->
+                Toast.makeText(
+                    applicationContext,
+                    e.localizedMessage,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
     }
 }
