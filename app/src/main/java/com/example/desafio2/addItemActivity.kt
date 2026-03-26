bf8ab6d26dd47f0c1a0aaea37fcdedcb5504e923
MR190768 <mr190768@alumno.udb.edu.sv>
2026-03-26 15:22:00 -0600

    ADD-activity de registro de lugares

--- app/src/main/java/com/example/desafio2/addItemActivity.kt
+++ app/src/main/java/com/example/desafio2/addItemActivity.kt
@@ -1,28 +1,32 @@
 package com.example.desafio2
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class Activity_menu : AppCompatActivity() {
+class addItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_add_item)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        findViewById<FloatingActionButton>(R.id.btn_addItem).setOnClickListener {
-            val intent = Intent(this, addItemActivity::class.java)
-            startActivity(intent)
+        val spinner: Spinner = findViewById(R.id.countries_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.Countries_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
         }
-
     }
 }
