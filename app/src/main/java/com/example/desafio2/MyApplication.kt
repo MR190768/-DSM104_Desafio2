e56870eca6f4a64ebfb4bd48936de349e8c086a2
MR190768 <mr190768@alumno.udb.edu.sv>
2026-03-28 18:00:29 -0600

    ADD-estilos onPressed a los botones

--- app/src/main/java/com/example/desafio2/MyApplication.kt
+++ app/src/main/java/com/example/desafio2/MyApplication.kt
@@ -7,9 +7,9 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         val config = mapOf(
-            "cloud_name" to "",
-            "api_key" to "",
-            "api_secret" to ""
+            "cloud_name" to "du0nm4hsj",
+            "api_key" to "864696861758682",
+            "api_secret" to "B5ujKoIv4DMV3xV3smUkrYI1OMQ"
         )
         MediaManager.init(this, config)
     }
